a4c181cf93d70256a27bf0ef347df9eca2143d70
odinSanliveira <odin.sanliveira@gmail.com>
2022-03-20 18:22:31 -0300

    adding initial projects files.

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './components/screens/HomeScreen'
+import { Container } from 'react-bootstrap'
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <HomeScreen/>
+        </Container>
+      </main>
+
+      <Footer/>      
+    </div>
+  );
+}
+
+export default App;
