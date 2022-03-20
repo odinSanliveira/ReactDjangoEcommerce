f0d7b18872155284cb1dffa2e5d64f0cf4217335
odinSanliveira <odin.sanliveira@gmail.com>
2022-03-20 18:48:48 -0300

    fixing folder paths, adding Product component and showing initial visual of products in home page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomeScreen from './components/screens/HomeScreen'
+import HomeScreen from './screens/HomeScreen'
 import { Container } from 'react-bootstrap'
 function App() {
   return (
