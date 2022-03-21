1a6d52ccb7d70fdfd050b0e75c16dcbdf98db049
odinSanliveira <odin.sanliveira@gmail.com>
2022-03-21 20:58:07 -0300

    adding initial settings of backend, configure routes and return Json respones with fake data to test, django rest framework configuration.

---
+++ backend/base/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .products import products
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/products/',
+        '/api/products/create/',
+
+        '/api/products/upload/',
+
+        '/api/products/<id>/reviews/',
+
+        '/api/products/top/',
+        '/api/products/<id>/',
+
+        '/api/products/delete/<id>/',
+        '/api/products/<update>/<id>/',
+    ]
+    return Response(routes)
+
+def getProducts(request):
+    return JsonResponse(products, safe=False)
