17b0afbf92c01ab4c71c2ad60c4fa8de1ba76825
odinSanliveira <odin.sanliveira@gmail.com>
2022-04-14 15:29:57 -0300

    serializing datas and connecting API to shop.

--- backend/base/views.py
+++ backend/base/views.py
@@ -5,7 +5,8 @@ from .products import products
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-
+from .models import Product
+from .serializers import ProductSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -27,13 +28,12 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProductsId(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
